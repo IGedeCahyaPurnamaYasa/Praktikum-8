1418a5d218ef85fb06546f63d80333527555df34
CahyaPurnama <cahya.purnama456@gmail.com>
2019-05-24 08:23:25 +0800

    database

--- database/buku.sql
+++ database/buku.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: 127.0.0.1
--- Generation Time: 27 Apr 2019 pada 19.42
+-- Generation Time: 23 Mei 2019 pada 18.43
 -- Versi Server: 10.1.13-MariaDB
 -- PHP Version: 5.6.20
 
@@ -39,10 +39,11 @@ CREATE TABLE `anggota` (
 --
 
 INSERT INTO `anggota` (`KdAnggota`, `Nama`, `Prodi`, `Jenjang`, `Alamat`) VALUES
-(1, 'Cahya', 'Informatika', 'S1', 'Kampus22'),
+(1, 'Cahya', 'Informatika', 'S1', 'Kampus222'),
 (2, 'asd', 'asd', 'asd', 'asd'),
 (4, 'ada', 'asd', 'asd', 'dasd'),
-(5, 'Siapa', 'Apa', '??', 'Dimana');
+(5, 'Siapa', 'Apa', '??', 'Dimana'),
+(6, 'asd', 'asd', 'dd', 'asd');
 
 -- --------------------------------------------------------
 
@@ -65,9 +66,7 @@ CREATE TABLE `buku` (
 INSERT INTO `buku` (`KdRegister`, `Judul`, `Pengarang`, `Penerbit`, `Thn_terbit`) VALUES
 (1, 'Buku Baru', 'Siapa', 'Siapa', 1980),
 (2, 'buku2', 'bu', 'bu22', 1990),
-(5, 'asd', 'asddd', 'asddd', 2005),
 (6, 'Buku Baru', 'Siapa', '123', 1980),
-(7, 'Buku Baru', 'Siapa', '11', 1980),
 (8, 'Bukukukukuku', 'ku', 'kuku', 2004);
 
 -- --------------------------------------------------------
@@ -95,7 +94,14 @@ INSERT INTO `detil_pinjam` (`KdPinjam`, `KdRegister`, `Tgl_pinjam`, `Tgl_kembali
 (5, 1, '0000-00-00', '2019-04-27'),
 (6, 2, '0000-00-00', '2019-04-27'),
 (7, 1, '2019-04-27', '0000-00-00'),
-(8, 5, '2019-04-27', '0000-00-00');
+(8, 5, '2019-04-27', '0000-00-00'),
+(9, 5, '2019-05-03', '0000-00-00'),
+(10, 8, '2019-05-03', '2019-05-03'),
+(11, 1, '2019-05-03', '2019-05-03'),
+(12, 1, '2019-05-03', '2019-05-03'),
+(13, 1, '2019-05-03', '2019-05-03'),
+(14, 1, '2019-05-03', '2019-05-03'),
+(15, 1, '2019-05-03', '2019-05-03');
 
 -- --------------------------------------------------------
 
@@ -106,6 +112,7 @@ INSERT INTO `detil_pinjam` (`KdPinjam`, `KdRegister`, `Tgl_pinjam`, `Tgl_kembali
 CREATE TABLE `peminjaman` (
   `KdPinjam` int(11) NOT NULL,
   `KdAnggota` int(11) NOT NULL,
+  `KdPetugas` int(11) NOT NULL,
   `KdRegister` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
@@ -113,15 +120,22 @@ CREATE TABLE `peminjaman` (
 -- Dumping data untuk tabel `peminjaman`
 --
 
-INSERT INTO `peminjaman` (`KdPinjam`, `KdAnggota`, `KdRegister`) VALUES
-(1, 1, 1),
-(2, 0, 1),
-(3, 0, 1),
-(4, 1, 0),
-(5, 1, 1),
-(6, 4, 2),
-(7, 1, 1),
-(8, 1, 5);
+INSERT INTO `peminjaman` (`KdPinjam`, `KdAnggota`, `KdPetugas`, `KdRegister`) VALUES
+(1, 1, 0, 1),
+(2, 0, 0, 1),
+(3, 0, 0, 1),
+(4, 1, 0, 0),
+(5, 1, 0, 1),
+(6, 4, 0, 2),
+(7, 1, 0, 1),
+(8, 1, 0, 5),
+(9, 1, 1, 5),
+(10, 1, 1, 8),
+(11, 1, 1, 1),
+(12, 1, 1, 1),
+(13, 1, 1, 1),
+(14, 1, 1, 1),
+(15, 1, 1, 1);
 
 -- --------------------------------------------------------
 
@@ -132,16 +146,21 @@ INSERT INTO `peminjaman` (`KdPinjam`, `KdAnggota`, `KdRegister`) VALUES
 CREATE TABLE `petugas` (
   `KdPetugas` int(11) NOT NULL,
   `Nama` varchar(100) NOT NULL,
-  `Alamat` varchar(200) NOT NULL
+  `Alamat` varchar(200) NOT NULL,
+  `username` varchar(50) NOT NULL,
+  `password` varchar(50) NOT NULL,
+  `last_login` date NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data untuk tabel `petugas`
 --
 
-INSERT INTO `petugas` (`KdPetugas`, `Nama`, `Alamat`) VALUES
-(1, 'Cahya', 'Rumah2'),
-(3, 'Siapa', 'Dimana');
+INSERT INTO `petugas` (`KdPetugas`, `Nama`, `Alamat`, `username`, `password`, `last_login`) VALUES
+(1, 'Cahya1', 'Rumah3', 'test1', 'test1', '2019-05-23'),
+(3, 'Siapa', 'Dimana', 'test2', 'test2', '2019-05-08'),
+(7, 'asd', 'ddas', '123', 'asd', '2019-05-03'),
+(8, 'Cahya Purnama ', 'rumah', 'cahya1', 'cahya1', '2019-05-23');
 
 --
 -- Indexes for dumped tables
@@ -185,7 +204,7 @@ ALTER TABLE `petugas`
 -- AUTO_INCREMENT for table `anggota`
 --
 ALTER TABLE `anggota`
-  MODIFY `KdAnggota` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+  MODIFY `KdAnggota` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 --
 -- AUTO_INCREMENT for table `buku`
 --
@@ -195,17 +214,17 @@ ALTER TABLE `buku`
 -- AUTO_INCREMENT for table `detil_pinjam`
 --
 ALTER TABLE `detil_pinjam`
-  MODIFY `KdPinjam` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+  MODIFY `KdPinjam` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=16;
 --
 -- AUTO_INCREMENT for table `peminjaman`
 --
 ALTER TABLE `peminjaman`
-  MODIFY `KdPinjam` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+  MODIFY `KdPinjam` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=16;
 --
 -- AUTO_INCREMENT for table `petugas`
 --
 ALTER TABLE `petugas`
-  MODIFY `KdPetugas` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `KdPetugas` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
